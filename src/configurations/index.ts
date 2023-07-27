73d486352bdc06a56c050d76e446076964301afb
Taras21071988 <tsasojlov@icloud.com>
2023-07-27 12:49:58 +0300

    add SequelizeModule

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -1,1 +1,8 @@
-export default () => ({ port: process.env.PORT });
+export default () => ({
+  port: process.env.PORT,
+  db_port: process.env.DB_PORT,
+  db_database: process.env.DB_DATABASE,
+  db_user: process.env.DB_USER,
+  db_password: process.env.DB_PASSWORD,
+  db_host: process.env.DB_HOST,
+});
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,12 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '../user/user.module';
 import { ImagesModule } from '../images/images.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import configuretions from '../../configurations';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configuretions] }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configServise: ConfigService) => ({
+        dialect: 'postgres',
+        host: configServise.get('db_host'),
+        port: configServise.get('db_port'),
+        username: configServise.get('db_user'),
+        password: configServise.get('db_password'),
+        database: configServise.get('db_name'),
+        synchronize: true,
+        autoLoadModels: true,
+        models: [],
+      }),
+    }),
     UserModule,
     ImagesModule,
   ],
