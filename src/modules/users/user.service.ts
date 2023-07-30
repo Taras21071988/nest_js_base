8f7b200964e6a2ef748542b1a226ee99b296017a
Taras21071988 <tsasojlov@icloud.com>
2023-07-30 16:06:25 +0300

    add update and delete user

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import * as bcrypt from 'bcrypt';
-import { CreateUserDTO } from './dto';
+import { CreateUserDTO, UpdateUserDTO } from './dto';
 import { BadRequestException } from '@nestjs/common';
 import { AppError } from 'src/common/constants/errors';
 
@@ -37,5 +37,15 @@ export class UserService {
       where: { email },
       attributes: { exclude: ['password'] },
     });
+  }
+  async updateUser(email: string, dto: UpdateUserDTO): Promise<UpdateUserDTO> {
+    await this.userRepository.update(dto, {
+      where: { email },
+    });
+    return dto;
+  }
+  async deleteUser(email: string) {
+    await this.userRepository.destroy({ where: { email } });
+    return true;
   }
 }
